c7706af496cefcd05a24f173139c85f205529ea4
Pedro Quezado <lucas.pedro.n@hotmail.com>
2017-06-14 19:46:39 -0300

    Update

--- array.php
+++ array.php
@@ -1,23 +1,32 @@
-<?php 
+<?php
 
-	$myArray = array();
 
-	array_push($myArray,"Pedro");
-	array_push($myArray,"Eric");
-	array_push($myArray,"Anita");
-	array_push($myArray,"Rose");
+	$pessoas = array();
 
-	sort($myArray);
-	var_dump($myArray);
-	echo "<br>";
-	rsort($myArray);
-	var_dump($myArray);
+	array_push($pessoas, "Pedro");
+	array_push($pessoas, "Eric");
+	array_push($pessoas, "Anita");
+	array_push($pessoas, "Rose");
+	array_push($pessoas, "Ruth");
+
+	sort($pessoas);
+
+	$quatidade = count($pessoas);
+	$rand = rand(0, $quatidade-1);
+
+	echo $pessoas[$rand];
 
 	echo "<br><hr><br>";
 
-	echo end($myArray);
+	foreach($pessoas as $pessoa){
+		echo $pessoa." ";
+	}
 
+	echo "<br><hr><br>";
 
-
-
- ?>
+	print_r($pessoas);
+	echo "<br>";
+	echo json_encode($pessoas);
+	echo "<br>";
+	$json = '["Anita","Eric","Pedro","Rose","Ruth"]';
+	echo json_decode($json);
